93c4a5556ab421215555bb45810ceff8dd2d2e1f
DeeptanshuSanghani1 <99564490+DeeptanshuSanghani@users.noreply.github.com>
2022-06-17 14:05:13 -0400

    version 1 - Initial project created

---
+++ ATMSimulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
